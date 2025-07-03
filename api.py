6be7dad4b52f36121b2eab73dfc6d120d267b5f5
梁俊涛 <liangjt@landray.com.cn>
2025-07-03 23:08:26 +0800

    初学设置log级别，打印log

--- api.py
+++ api.py
@@ -1,8 +1,12 @@
 """
 程序启动入口
 """
-from flask import Flask, request
+from flask import Flask, request, logging
+
 app = Flask(__name__)
+
+# 设置全局日志级别（DEBUG/INFO/WARNING/ERROR/CRITICAL）
+app.logger.setLevel("INFO")
 @app.route('/hello')
 def hello_world():
     """
@@ -11,6 +15,7 @@ def hello_world():
     # 这是一个不可变的字典
     params = request.args
     name = params.get('name',default='Lnn')
+    app.logger.info(f'name: {name}')
     return f'Hello, World! {name}'
 
 @app.route('/login',methods=['POST'])
@@ -20,10 +25,11 @@ def login():
     """
     username = request.form.get('username',default='')
     password = request.form.get('password',default='')
+    app.logger.info(f"login user: {username}")
     return {
         'username':username,
         'password':password
     }
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost', port=10010)
